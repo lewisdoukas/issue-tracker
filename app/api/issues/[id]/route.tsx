32046b60d04005f076e751a6f4fc2f66b5f773b8
Ilias Doukas <doukas.lewis@gmail.com>
2024-07-16 16:58:55 +0300

    Secure the application

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -1,11 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import { issueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
+import authOptions from "@/app/auth/authOptions";
+import { getServerSession } from "next-auth";
 
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({}, { status: 401 });
+  }
+
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
 
@@ -38,6 +46,12 @@ export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({}, { status: 401 });
+  }
+
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
