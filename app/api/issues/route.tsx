b6b5bd16080798e03d1a9d982f49b0609c218c68
Ilias Doukas <doukas.lewis@gmail.com>
2024-07-16 23:01:24 +0300

    Build layout of pagination component

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -5,11 +5,11 @@ import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
 
 export async function POST(request: NextRequest) {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
-  if (!session) {
-    return NextResponse.json({}, { status: 401 });
-  }
+  // if (!session) {
+  //   return NextResponse.json({}, { status: 401 });
+  // }
 
   const body = await request.json();
 
